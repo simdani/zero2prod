fef6e6cc2648dfb2e71c09ec3b68cb2c7a513539
Simonas Daniliauskas <daniliauskas.s@gmail.com>
2021-10-30 22:39:05 +0300

    implement telemetry: tracing

---
+++ src/telemetry.rs
@@ -0,0 +1,29 @@
+use tracing::subscriber::set_global_default;
+use tracing::Subscriber;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::fmt::MakeWriter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
+
+pub fn get_subscriber(
+    name: String,
+    env_filter: String,
+    sink: impl MakeWriter + Send + Sync + 'static,
+) -> impl Subscriber + Send + Sync {
+    // We are falling back to printing all spans at info-level or above if RUST_LOG env has not been set.
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
+
+    return Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+}
+
+// Register a subscriber as global default to process span data.
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
